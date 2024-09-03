74bba9d8abf774dadfe2e0051e2d7a5ae8eeb677
miles-o <miles@noisyoyster.net>
2024-09-03 12:38:58 +1000

    Website update - API putting time in correct places, formatting still needed

--- HTML/Assessment2/javascript/race-tile.js
+++ HTML/Assessment2/javascript/race-tile.js
@@ -1,4 +1,13 @@
-function createRaceTile(row, compId) {
+function getSessionTime(session, currentEvent) {
+    // let tempString = currentEvent.get(session).time;
+    // tempString = tempString.split('+');
+    let sessionTime = new Date(currentEvent.get(session).date);
+    return sessionTime.toLocaleTimeString("en-AU") + " " + sessionTime.toLocaleDateString("en-AU");
+}
+
+function createRaceTile(row, compId, events) {
+    let currentEvent = events.get(compId);
+
     const raceTile = document.createElement("div");
     raceTile.className = "race-tile";
 
@@ -7,7 +16,7 @@ function createRaceTile(row, compId) {
 
     const trackName = document.createElement("h4");
     trackName.className = "track-name";
-    trackName.innerText = "Track name";
+    trackName.innerText = currentEvent.get("Race").circuit.name;
 
     const horizontalContent = document.createElement("div");
     horizontalContent.className = "horizontal-content";
@@ -17,26 +26,26 @@ function createRaceTile(row, compId) {
 
     const p1 = document.createElement("p");
     p1.className = "p1";
-    p1.innerText = "P1: ";
+    p1.innerText = "P1: " + getSessionTime("1st Practice", currentEvent);
 
     const p2 = document.createElement("p");
     p2.className = "p2";
-    p2.innerText = "P2: ";
+    p2.innerText = "P2: " + getSessionTime("2nd Practice", currentEvent);
 
     const p3 = document.createElement("p");
     p3.className = "p3";
-    p3.innerText = "P3: ";
+    p3.innerText = "P3: " + getSessionTime("3rd Practice", currentEvent);
 
     const verticalContent2 = document.createElement("div");
     verticalContent2.className = "vertical-content";
 
     const quali = document.createElement("p");
     quali.className = "quali";
-    quali.innerText = "Quali: ";
+    quali.innerText = "Quali: " + getSessionTime("1st Qualifying", currentEvent);
 
     const race = document.createElement("p");
     race.className = "race";
-    race.innerText = "Race: ";
+    race.innerText = "Race: " + getSessionTime("Race", currentEvent);
 
     raceTile.appendChild(raceTileInfo);
     raceTileInfo.appendChild(trackName);
@@ -70,7 +79,7 @@ fetch(url1, options)
         return answer.json();
     })
     .then((answer) => {
-        console.log(answer);
+
         let events = new Map();
         for (let i = 0; i < answer.response.length; i++) {
             let compId = answer.response[i].competition.id;
@@ -80,27 +89,27 @@ fetch(url1, options)
             if (events.get(compId) !== undefined) {
                 tempEvent = events.get(compId);
             }
-            tempEvent.set(type, answer.response[i]);
-
+            tempEvent.set(String(type), answer.response[i]);
             events.set(compId, tempEvent);
         }
-        console.log(events);
+
         let row = 1;
         let col = 0;
-        for (let i = 0; i < compIds.length; i++) {
+        let keys = events.keys();
+        for (let i = 0; i < events.size; i++) {
+            let compId = keys.next();
             if (col < 3) {
-                createRaceTile(row, compIds[i]);
+                createRaceTile(row, compId.value, events);
                 col++;
             } else {
                 col = 1;
                 row++;
-                console.log(row);
                 const newRow = document.createElement("div");
                 newRow.className = "horizontal-content";
                 newRow.id = "row-" + row;
                 const element = document.getElementById("tile-holder");
                 element.appendChild(newRow);
-                createRaceTile(row, compIds[i]);
+                createRaceTile(row, compId.value, events);
             }
 
         }
